169f4e84b7a96a53a71d859f54c77c941f412a2a
Saqib-wpbrigade <saqib.wpbrigade@gmail.com>
2022-12-28 16:21:46 +0500

    post widget

--- widgets/widget-slider.php
+++ widgets/widget-slider.php
@@ -127,10 +127,36 @@ class Elementor_slider_Widget extends \Elementor\Widget_Base
 		$repeater->add_control(
 			'list_link',
 			[
-				'label' => __('Link', 'widget-test'),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => __('#', 'widget-test'),
-				'show_label' => true,
+				
+				'label' => esc_html__( 'Link', 'widget-test' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'placeholder' => esc_html__( 'https://your-link.com', 'textdomain' ),
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+			]
+		);
+		$repeater->add_control(
+			'heading_tag',
+			[
+				
+				'label' => esc_html__( 'Title tag', 'widget-test' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'options' => [
+					'h1' => 'H1',
+					'h2' => 'H2',
+					'h3' => 'H3',
+					'h4' => 'H4',
+					'h5' => 'H5',
+					'h6' => 'H6',
+					'div' => 'div',
+					'span' => 'span',
+					'p' => 'p',
+				],
+				'default' => 'h3',
 			]
 		);
 
@@ -231,53 +257,57 @@ class Elementor_slider_Widget extends \Elementor\Widget_Base
 		if ($settings['slider_widget']) {
 
 			?>
-			<!-- Start rendering the output -->
-			<div class="stories-slider-wrapper">
-				<div class="container">
-					<div class="story-slider-wrp">
-						<div class="swiper swiper-container">
-							<div class="swiper-wrapper">
-								<?php foreach ($settings['slider_widget'] as $index => $item) : ?>
-									<?php
+<!-- Start rendering the output -->
+<div class="stories-slider-wrapper">
+    <div class="container">
+        <div class="story-slider-wrp">
+            <div class="swiper swiper-container">
+                <div class="swiper-wrapper">
+                    <?php foreach ($settings['slider_widget'] as $index => $item) : ?>
+                    <?php
 									$image_url = isset($item['slider_image']['url']) && !empty($item['slider_image']['url']) ? $item['slider_image']['url']  : false;
 									$title = isset($item['slider_title']) && !empty($item['slider_title']) ? $item['slider_title'] : false;
 									$button_text = isset($item['button_text']) && !empty($item['button_text']) ? $item['button_text'] : false;
-									?>
-									<div class="swiper-slide">
-										<div class="slide-story">
-											<div class="featured-img">
-												<?php if ($image_url) : ?>
-													<?php echo '<img src="' . esc_url($image_url) . '" alt="">'; ?>
-												<?php endif; ?>
-											</div>
-											<div class="content-col-slide">
-												<?php if ($title) : ?>
-													<h4 class="name-heading"><?php echo  $title ?></h4>
-												<?php endif; ?>
-												<div class="description"><?php echo $item['slider_description'] ?></div>
-												<div class="btn-wrp text-link">
-													<?php if ($button_text) : ?>
-														<a href="<?php echo $item['list_link']; ?>"><?php echo $button_text ?></a>
-													<?php endif; ?>
+									$titleSizeTag=isset($item['heading_tag']) ? $item ['heading_tag'] : false; 
+									
+					?>
+                    <div class="swiper-slide">
+                        <div class="slide-story">
+                            <div class="featured-img">
+                                <?php if ($image_url) : ?>
+                                <?php echo '<img src="' . esc_url($image_url) . '" alt="">'; ?>
+                                <?php endif; ?>
 
-												</div>
-											</div>
-										</div>
-									</div>
-								<?php endforeach; ?>
-							</div>
-							<div class="swiper-controller">
-								<div class="swiper-next-btn"></div>
-								<div class="swiper-prev-btn"></div>
-								<div class="swiper-pagination"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- End rendering the output -->
+                            </div>
+                            <div class="content-col-slide">
+                                <?php if ($title) : ?>
+								<?php echo '<' .$titleSizeTag. '>' .$title.'</' .$titleSizeTag. '> ' ; ?>
+                                
+                                <?php endif; ?>
+                                <div class="description"><?php echo $item['slider_description'] ?></div>
+                                <div class="btn-wrp text-link">
+                                    <?php if ($button_text) : ?>
+                                    <a href="<?php echo esc_url($item['list_link']['url']); ?>" target="<?php echo ($item['list_link']['is_external']) ? "_blank" : '' ?>"><?php echo $button_text ?></a>
+                                    <?php endif; ?>
 
-			<?php
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="swiper-controller">
+                    <div class="swiper-next-btn"></div>
+                    <div class="swiper-prev-btn"></div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End rendering the output -->
+
+<?php
 		}
 	}
 }
